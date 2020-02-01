337c91eb1a191fc4bc9b6dbd798861d59c9ff3b6
K00lman <57235429+K00lmans@users.noreply.github.com>
2020-01-31 20:26:40 -0500

    Add files via upload

--- Search Engine Project/Search Engine.py
+++ Search Engine Project/Search Engine.py
@@ -2,19 +2,30 @@ import os
 
 
 def inputs() -> [str, int]:
+    """Takes the constraints from the user for the search"""
     term = input("Please enter your search term: ")
+    # Asks for the number of results the the user would like to see, and makes sure it is an int
     while True:
         try:
             result_count = int(input("How many results would you like to find: "))
             break
-        except:
-            print("Please enter a number")
-    return(term, result_count)
+        except ValueError:
+            print("Please enter only a whole number")
+    return term, result_count
 
 
 def data_to_dictionary() -> dict:
+    """Turns the raw data in the text files into a dict"""
     data = {}
-    # I do not know how this code segment works.
+    # Creates a list(?) of all the file names in the specified folder,
+    # then opens those files and put the name and data of those files in a dict
     for root, dirs, files in os.walk("Data"):
-        for filename in files:
-            data[filename[:-4]] = "data"
+        for file_name in files:
+            with open(".\\Data\\" + file_name) as file:
+                file_data = file.read()
+            data[file_name[:-4]] = file_data  # The [:-4] is to remove the .txt extension
+    return data
+
+
+term, result_count = inputs()
+data = data_to_dictionary()
